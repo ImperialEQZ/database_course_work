a0a33d82ba9383bb06043355566df17cbc5cf2aa
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-12-15 14:55:23 +0300

    Добавлена настройка подключения к БД

---
+++ config/database.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        logging: false
+    }
+);
+
+// Проверка подключения
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(' Успешное подключение к базе данных PostgreSQL');
+    } catch (error) {
+        console.error('Ошибка: невозможно подключиться к базе данных:', error);
+    }
+})();
+
+module.exports = sequelize;
