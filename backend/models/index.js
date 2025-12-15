b71699d92083badff9a63ef997e2424959943c2b
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-12-15 16:06:13 +0300

    Файл для связи всех моделей

---
+++ backend/models/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const sequelize = require('/config/database');
+const db = {};
+
+// Импорт всех моделей из папки
+fs.readdirSync(__dirname)
+    .filter(file => file !== 'index.js' && file.endsWith('.js'))
+    .forEach(file => {
+        const model = require(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+// Установка связей между моделями
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+module.exports = db;
