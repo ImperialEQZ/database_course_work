164a5e88c174b41b7a0b8305110e2c000e82da50
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-12-15 15:18:32 +0300

    создание моделей для PostgreSQL

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('database_course_work/config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('admin', 'user'),
+        defaultValue: 'user',
+        allowNull: false
+    }
+}, {
+    tableName: 'users',
+    timestamps: false
+});
+
+User.associate = function(models) {
+    User.hasMany(models.Order, {
+        foreignKey: 'users_id',
+        as: 'orders'
+    });
+};
+
+module.exports = User;
