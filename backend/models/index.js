1fb9f069da336f6fe0df522dde8ee17ba2912f79
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-12-15 17:52:26 +0300

    соединение всех моделей

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const sequelize = require('/config/database');
+const sequelize = require('../../config/database');
 const db = {};
 
 // Импорт всех моделей из папки
